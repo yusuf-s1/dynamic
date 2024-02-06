b42cbdc84c83221bb835c12b75d1ccd9d69002d4
Yusuf Solmaz <yusufsolm4z@gmail.com>
2024-02-06 23:32:04 +0300

    updated mobile sidebar

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 let main = document.querySelector('.main');
 let colorOptions = document.querySelectorAll('.sidebar-color-option');
-
+let sidebar = document.querySelector('.sidebar');
+let customizeBtn = document.querySelector('.customize-btn');
 for (let index = 0; index < colorOptions.length; index++) {
   colorOptions[index].style.backgroundColor = colorOptions[index].value;
 }
